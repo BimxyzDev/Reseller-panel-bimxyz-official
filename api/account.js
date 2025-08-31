91934b190756d192b59401430f0b572dc9efa737
BimxyzDev <bimaputra436123@gmail.com>
2025-08-31 13:10:12 +0700

    Update account.js

--- api/account.js
+++ api/account.js
@@ -2,6 +2,7 @@
 export const accounts = [
   { username: "admin", password: "admin089654" },
   { username: "rapz12", password: "rapz321" },
+  { username: "tes", password: "123"}
 ];
 
 export function validateLogin(username, password) {
