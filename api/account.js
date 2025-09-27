b96ce1ae1c59360944967a1e6f56bee4ce367c32
BimxyzDev <bimaputra436123@gmail.com>
2025-09-27 19:27:35 +0700

    Update account.js

--- api/account.js
+++ api/account.js
@@ -1,31 +1,6 @@
 // api/account.js
 export const accounts = [
   { username: "admin", password: "admin089654" },
-  { username: "rapz12", password: "rapz321" },
-  { username: "Alpan", password: "Alpan354" },
-    { username: "mamz", password: "mamz9772" },
-  { username: "amlv", password: "amlv08977" },
-  { username: "Lans", password: "Lans0" },
-  { username: "rexz", password: "bebas" },
-    { username: "sinz", password: "sinz086658" },
-  { username: "Payzz", password: "awok08977" },
- { username: "Fan", password: "Fan089777" },
-  { username: "vidzx", password: "vidzx089777" },
-  { username: "adityagntng", password: "adit22376" },
-    { username: "Ichigo", password: "Ichigo8762" },
-  { username: "ADITZG", password: "ADITZG8762" },
-  { username: "YIZZD", password: "YIZZD77647" },
-  { username: "RAIZO", password: "RAIZO9847" },
-  { username: "wahyuu22", password: "wahyuu2247" },
-  { username: "ashurammk", password: "ashurammk889" },
-    { username: "nopal", password: "nopal6387" },
-    { username: "Ocim", password: "Ocim6651" },
-  { username: "adminkxa", password: "kxateam" },
-    { username: "Revan", password: "Revan3044" },
-  { username: "adminch", password: "adminch5282" },
-  { username: "Akmal", password: "Akmal883" },
-    { username: "wendy", password: "wendy708" },
-  { username: "reegie", password: "reegie5053" },
   
 ];
 
