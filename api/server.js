bb3ce16a10773a3bc646cb1c6639f0d1a08a62dd
BimxyzDev <bimaputra436123@gmail.com>
2025-10-06 18:21:46 +0700

    Update server.js

--- api/server.js
+++ api/server.js
@@ -2,8 +2,8 @@
 import { validateLogin } from './account';
 
 export default async function handler(req, res) {
-  const PANEL_URL = "https://alifserver.privateserverr.my.id";
-  const API_KEY   = "ptla_k1vMsecicE4blsPaoOFY7kqEfv9yL4oPTYDFMekDkwH";
+  const PANEL_URL = "https://panelpublicrusuhh.resellergaming-official.my.id";
+  const API_KEY   = "ptla_18qeId6XM7dWAZOUyM6DTRQiVDicaApEDv6zEFAGFyM";
   const NODE_ID   = 1;
   const EGG_ID    = 15;
   const DOCKER_IMG = "ghcr.io/parkervcp/yolks:nodejs_24";
