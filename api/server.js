1a489f2e734e6b54262497a3cc431125d69f9fa5
BimxyzDev <bimaputra436123@gmail.com>
2025-10-21 14:35:59 +0700

    Update server.js

--- api/server.js
+++ api/server.js
@@ -2,8 +2,8 @@
 import { validateLogin } from './account';
 
 export default async function handler(req, res) {
-  const PANEL_URL = "https://masvilsukafritzy.xpanelprivate.my.id";
-  const API_KEY   = "ptla_qGEMgC5DWdZXnNSqwG1SFdaeXelJnMQSwlxrgI4Ohfn";
+  const PANEL_URL = "https://manxz-zarrrpanel.publicserverr.my.id";
+  const API_KEY   = "ptla_HoVSunl4reXNRMYk7fj57RksOkI4ynQEeKzZ0LJjau5";
   const NODE_ID   = 1;
   const EGG_ID    = 15;
   const DOCKER_IMG = "ghcr.io/parkervcp/yolks:nodejs_21";
