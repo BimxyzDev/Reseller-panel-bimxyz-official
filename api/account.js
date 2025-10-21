9660e356cb98f528dfd7445cb28959af2d12e6a5
BimxyzDev <bimaputra436123@gmail.com>
2025-10-21 18:31:44 +0700

    Update account.js

--- api/account.js
+++ api/account.js
@@ -1,28 +1,35 @@
 // api/account.js
+export const accounts = [
+  { username: "admin", password: "admin089654" },
+  { username: "ashurammk", password: "ashurammk889" },
+  { username: "VANZZ", password: "vanzz" },
+{ username: "ICHIGO", password: "ICHIGO001" },
+{ username: "Akmal", password: "1707" },
+{ username: "wendy", password: "wendy708" },
+{ username: "sinz", password: "sinz001" },
+{ username: "PAYZZ", password: "PAYZZ27" },
+{ username: "adminch", password: "adminch5282" },
+{ username: "adityagntng", password: "adit22376" },
+{ username: "Revan", password: "Revan3044" },
+{ username: "valltamvan", password: "1781945" },
+{ username: "LANS", password: "LANSAJAH" },
+{ username: "Yizzd", password: "462012" },
+{ username: "rawrganteng", password: "rawrxrxtzy" },
+{ username: "ALSTORE", password: "alnope22" },
+{ username: "nopal", password: "nopal1987" },
+{ username: "aditya store", password: "aditya522PU" },
+{ username: "ress", password: "ress66" },
+{ username: "reegie", password: "reegie717" },
+{ username: "SEMPAKABU²", password: "BOLONG" },
+{ username: "kyfan", password: "kyfan11" },
+{ username: "Alvaz_str", password: "alvaz_str" },
+{ username: "Davv", password: "naruto123" },
+  { username: "VANZZ", password: "vanzz" },
+  { username: "HENZZ_GX", password: "hendri123" },
 
-import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
+  
+];
 
-const SUPABASE_URL = "https://sashqayrkigdndzuxrqq.supabase.co";
-const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNhc2hxYXlya2lnZG5kenV4cnFxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjEwMzc1NDksImV4cCI6MjA3NjYxMzU0OX0.enllEu8g07833MZSxztXcEab4FewFpLBFkZkfVwbjr8";
-
-const db = createClient(SUPABASE_URL, SUPABASE_KEY);
-
-export async function validateLogin(username, password) {
-  try {
-    const { data, error } = await db
-      .from("accounts") // ← nama tabel di Supabase
-      .select("*")
-      .eq("username", username)
-      .eq("password", password);
-
-    if (error) {
-      console.error("Supabase error:", error);
-      return false;
-    }
-
-    return data && data.length > 0;
-  } catch (err) {
-    console.error("Error saat login:", err);
-    return false;
+export function validateLogin(username, password) {
+  return accounts.some(acc => acc.username === username && acc.password === password);
   }
-}
