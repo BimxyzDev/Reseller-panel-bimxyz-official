330068c8a0d73e260966aafd10106d6251e1b915
BimxyzDev <bimaputra436123@gmail.com>
2025-10-22 18:04:53 +0700

    Update and rename supabase.js to account.js

--- api/supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const SUPABASE_URL = "https://sashqayrkigdndzuxrqq.supabase.co";
-const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNhc2hxYXlya2lnZG5kenV4cnFxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjEwMzc1NDksImV4cCI6MjA3NjYxMzU0OX0.enllEu8g07833MZSxztXcEab4FewFpLBFkZkfVwbjr8";
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
