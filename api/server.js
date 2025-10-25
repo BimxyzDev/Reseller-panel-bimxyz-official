1a87907dd49d535ac2c3143a3a24d270c8fc747d
BimxyzDev <bimaputra436123@gmail.com>
2025-10-25 19:01:07 +0700

    Update server.js

--- api/server.js
+++ api/server.js
@@ -2,8 +2,8 @@
 import { validateLogin } from './account';
 
 export default async function handler(req, res) {
-  const PANEL_URL = "https://server-pterodactyl.kingstoree.web.id";
-  const API_KEY   = "ptla_rePZ37vAPSBvrxsCodBn1UYY3RXPQzamM5dhS6rGUfg";
+  const PANEL_URL = "https://alluffyhosting.new.server.lightsecretaja.web.id";
+  const API_KEY   = "ptla_eJCNrMyShlOJYWABCOrYMBtXT5o6ZPv3dv1YK49a56D";
   const NODE_ID   = 1;
   const EGG_ID    = 15;
   const DOCKER_IMG = "ghcr.io/parkervcp/yolks:nodejs_20";
