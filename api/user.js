c01841f8ebc93527085cfb2c1839a01ea3f95d39
BimxyzDev <bimaputra436123@gmail.com>
2025-11-16 12:01:31 +0700

    Create user.js

---
+++ api/user.js
@@ -0,0 +1,6 @@
+// api/user.js
+export const users = [
+  { username: "Admin", password: "Admin089" },
+  { username: "User1", password: "Pass123" },
+  { username: "User2", password: "Password2" },
+];
