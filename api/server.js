2e20630dc5de537b8c441be6dd5aa19ef098784f
BimxyzDev <bimaputra436123@gmail.com>
2025-11-16 13:33:54 +0700

    Update server.js

--- api/server.js
+++ api/server.js
@@ -2,8 +2,8 @@
 import { validateLogin } from './account';
 
 export default async function handler(req, res) {
-  const PANEL_URL = "https://panel.lu.com";
-  const API_KEY   = "ptla_panellu";
+  const PANEL_URL = "https://azizxofficial.alyabotz.my.id";
+  const API_KEY   = "ptla_xFZo3kycrK17mJdcR7wseCMm8Vur9KTDfzbsK1q7gpD";
   const NODE_ID   = 1;
   const NEST_ID   = 5;
   const EGG_ID    = 15;
@@ -44,7 +44,7 @@ export default async function handler(req, res) {
       }
 
       if (action === "create") {
-        const email = `user${Date.now()}@mail.com`;
+        const email = `user${Date.now()}@buyer.bimxyz.com`;
         const userPassword = Math.random().toString(36).slice(-8);
 
         // Buat user baru
