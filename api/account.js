6d69ce7041c978a3af9714f9e83ecf0ca8fc3fbe
BimxyzDev <bimaputra436123@gmail.com>
2025-11-16 12:00:43 +0700

    Update account.js

--- api/account.js
+++ api/account.js
@@ -1,6 +1,8 @@
-accounts = [
-  {
-    "username": "Admin",
-    "password": "Admin089"
-  }
-];
+// api/account.js
+import { users } from './user.js';
+
+export const accounts = users;
+
+export function validateLogin(username, password) {
+  return accounts.some(acc => acc.username === username && acc.password === password);
+}
