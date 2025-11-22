81df37ee5005b824e6cf67f1d139fd0becd2154f
BimxyzDev <bimaputra436123@gmail.com>
2025-11-22 14:25:48 +0700

    Update server.js

--- api/server.js
+++ api/server.js
@@ -3,7 +3,7 @@ import { validateLogin } from './account';
 
 export default async function handler(req, res) {
   const PANEL_URL = "https://kenz-ai.zayprivate.my.id";
-  const API_KEY   = "ptla_WEjPPuwO4flDzah5JrBJiIeIckqcCnVY9XVazI5syLK";
+  const API_KEY   = "ptla_i0lIIW8QwXpDxYKUKQFx3NNYd7xbpnOjwzcypvHBtz6";
   const NODE_ID   = 1;
   const NEST_ID   = 5;
   const EGG_ID    = 15;
