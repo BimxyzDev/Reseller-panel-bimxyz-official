3c7c0a7dc78f2e974537938e60ac00c25a9ea0b0
BimxyzDev <bimaputra436123@gmail.com>
2025-12-02 12:14:33 +0700

    Create panel.js

---
+++ api/panel.js
@@ -0,0 +1,8 @@
+// api/panel.js
+
+export const PANEL_URL  = "https://azizganteng.mbbstore.my.id";
+export const API_KEY    = "ptla_wJtvUAQLFJV45BBXvIvqeMgbMcnRv6NB5DP8IB8LkEk";
+export const NODE_ID    = 1;
+export const NEST_ID    = 5;
+export const EGG_ID     = 15;
+export const DOCKER_IMG = "ghcr.io/parkervcp/yolks:nodejs_20";
