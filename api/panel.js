3eb4af67757eb9097efa9256ba3e91e65a53957a
BimxyzDev <bimaputra436123@gmail.com>
2025-12-02 22:44:17 +0700

    Update panel.js

--- api/panel.js
+++ api/panel.js
@@ -1,7 +1,7 @@
 // api/panel.js
 
-export const PANEL_URL  = "https://azizganteng.mbbstore.my.id";
-export const API_KEY    = "ptla_wJtvUAQLFJV45BBXvIvqeMgbMcnRv6NB5DP8IB8LkEk";
+export const PANEL_URL  = "https://mypanel.bimxyz.my.id";
+export const API_KEY    = "ptla_lb5ZNamaBahac3Kx6vSamaS314IBkvn6wZ5TflDP4qU";
 export const NODE_ID    = 1;
 export const NEST_ID    = 5;
 export const EGG_ID     = 15;
