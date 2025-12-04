d2bee51b0591e902adb2604277daf7670eb92144
BimxyzDev <bimaputra436123@gmail.com>
2025-12-04 16:58:00 +0700

    update panel settings

--- api/panel.js
+++ api/panel.js
@@ -1,8 +1,6 @@
-// api/panel.js
-
-export const PANEL_URL  = "https://panelprivate.bimxyz.my.id";
-export const API_KEY    = "ptla_bXiNfiOBsO8cjC5DSuMNw6Kwjw6AZHREwwpb1XZJKFF";
-export const NODE_ID    = 1;
-export const NEST_ID    = 5;
-export const EGG_ID     = 15;
-export const DOCKER_IMG = "ghcr.io/parkervcp/yolks:nodejs_20";
+export const PANEL_URL = "https://panelprivate.bimxyz.my.id";
+export const API_KEY = "ptla_bXiNfiOBsO8cjC5DSuMNw6Kwjw6AZHREwwpb1XZJKFF";
+export const NODE_ID = "1";
+export const NEST_ID = "5";
+export const EGG_ID = "15";
+export const DOCKER_IMG = "ghcr.io/parkervcp/yolks:nodejs_21";
