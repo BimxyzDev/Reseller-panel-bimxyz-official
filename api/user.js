a2fed8c31653f01af68baa38d46332752c3a9b5e
BimxyzDev <bimaputra436123@gmail.com>
2025-12-05 15:39:03 +0700

    update users

--- api/user.js
+++ api/user.js
@@ -8,10 +8,6 @@ export const users = [
     "password": "gantengnjir"
   },
   {
-    "username": "Sementara",
-    "password": "321"
-  },
-  {
     "username": "sinz",
     "password": "sinz01"
   },
